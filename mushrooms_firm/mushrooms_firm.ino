9578ce61f77c58f8e2657bea7495e5ffee46cb77
Léo Marché <leo.marche@grenoble-inp.fr>
2022-12-10 00:52:39 +0100

    added water atomized to the code

--- mushrooms_firm/mushrooms_firm.ino
+++ mushrooms_firm/mushrooms_firm.ino
@@ -26,12 +26,6 @@ TH06_dev TH06;
 TH06_dev TH06;
 #endif
 
-/*byte temprcvByte[5];
-int tmpSensor = 2;
-int analogPin = A0;
-int analogVal = 0;
-*/
-
 int PWM_tmp = 10;
 int PWM_hygro = 11;
 float integraleTmpError, integraleHygroError;
@@ -44,8 +38,6 @@ rgb_lcd lcd;
 float ct = 0;
 
 void setup() {
-   /*pinMode(tmpSensor, OUTPUT);
-   digitalWrite(tmpSensor, HIGH);*/
    pinMode(PWM_tmp, OUTPUT);
    pinMode(PWM_hygro, OUTPUT);
    TH06.begin();
@@ -61,23 +53,6 @@ void setup() {
 }
 
 void loop() {
-  /*
-   dht(tmpSensor);
-   Serial.print("Humdity 1 = ");
-  Serial.print(temprcvByte[0], DEC);
-  Serial.print(".");
-  Serial.print(temprcvByte[1], DEC);
-  Serial.println("%");
-  Serial.print("Temperature 1 = ");
-  Serial.print(temprcvByte[2]-18, DEC);
-  Serial.print(".");
-  Serial.print(temprcvByte[3], DEC);
-  Serial.println(" degC");
-  delay(100);
-
-  Serial.println(temprcvByte[0]+temprcvByte[1]+temprcvByte[2]+temprcvByte[3]);
-  Serial.println(temprcvByte[4]);
-  */
 
   if(t==0){
     t = millis();
@@ -88,27 +63,10 @@ void loop() {
   retourHygro = TH06.ReadHumidity();
 
   Serial.println(retourTmp);
-
   lcd.clear();
-  /*
-  float rgb[3] = {0,0,0};
-  hsv2rgb(ct, 1.0, 255, rgb);
-  lcd.setRGB((int) rgb[0], (int) rgb[1], (int) rgb[2]);
-  ct += 0.01;
-  */
   lcd.setCursor(0, 0);
   lcd.print(retourTmp);             // ou lcd.print(nom de la variable à afficher);
 
-  /*
-  ShowSerial.print("Temperature: ");
-  ShowSerial.print(retourTmp);
-  ShowSerial.println("C");
-  //Serial.print("%\t");
-
-  ShowSerial.print("Humidity: ");
-  ShowSerial.print(retourHygro);
-  ShowSerial.println("%");
-  */
   float currt = millis();
 
   // Get dt
@@ -124,17 +82,9 @@ void loop() {
 
   setTmpCommand();
   setHygroCommand();
-
-  /*
-  ShowSerial.print("CommandTemperature: ");
-  ShowSerial.println(commandTmp);
-  ShowSerial.print("CommandHygro: ");
-  ShowSerial.println(commandHygro);
-  */
-
-  delay(1000);
 }
 
+/*
 float fract(float x) { return x - int(x); }
 
 float mix(float a, float b, float t) { return a + (b - a) * t; }
@@ -147,6 +97,7 @@ float* hsv2rgb(float h, float s, float b, float rgb[3]) {
   rgb[2] = b * mix(1.0, constrain(abs(fract(h + 0.3333333) * 6.0 - 3.0) - 1.0, 0.0, 1.0), s);
   return rgb;
 }
+*/
 
 float pid(float coeffProp, float coeffInt, float error, float *integraleError, float dt) {
   (*integraleError) += error * dt;
@@ -161,7 +112,6 @@ float limit(float commandValue){
   } else if(commandValue > 5){
     commandValue = 5;
   }
-
   return commandValue;
 }
 
@@ -173,37 +123,9 @@ void setTmpCommand() {
 }
 
 void setHygroCommand() {
-  // C'mon, do smthing
+  int timeUp = commandHygro / 16;
+  digitalWrite(PWM_hygro, HIGH);
+  delay(timeUp*100);
+  digitalWrite(PWM_hygro, LOW);
+  delay((16-timeUp)*100);
 }
-
-/*
-byte getTmpByteValue(int pin) {
-  byte i = 0;
-  byte value = 0;
-  for (i = 0; i < 8; i++) {
-    while (digitalRead(pin) == LOW); // wait for pin state to be HIGH
-    delayMicroseconds(35);          // wait 30 microSeconds
-    if (digitalRead(pin) == HIGH)
-      value |= (1 << (8 - i));      // save bit value if pin 10 is HIGH
-    while (digitalRead(pin) == HIGH); // wait for pin state to be LOW
-  }
-  return value;
-}
-
-void dht(int pin) {
-  digitalWrite(pin, LOW);  // set pin LOW to start communication with module
-  delay(30);              // wait 30 milliSeconds
-  digitalWrite(pin, HIGH); // set pin HIGH
-  delayMicroseconds(20);  // wait 20 microSeconds
-  pinMode(pin, INPUT);     // change pin 10 mode to input
-  while (digitalRead(pin) == HIGH);    // wait for pin to be LOW
-  delayMicroseconds(50);  // wait for 80 microSeconds
-  while (digitalRead(pin) == LOW);    // wait for pin to be HIGH
-  delayMicroseconds(50); // wait for 80 microSeconds
-  while (digitalRead(pin) == HIGH);    // wait for pin to be LOW
-  for (int i = 0; i < 5; i++) // receive temperature and humidity values
-    temprcvByte[i] = getTmpByteValue(pin);
-  pinMode(pin, OUTPUT);    // change pin 10 mode to ouptut
-  digitalWrite(pin, HIGH); // set pin 10 to HIGH
-}
-*/
